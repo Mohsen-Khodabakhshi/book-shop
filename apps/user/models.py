18e323a7f9854c49c8997774de49908fb20b039a
Mohsen Khodabakhshi <mim.khodabakhshi@gmail.com>
2022-05-17 20:12:16 +0430

    User and Address models

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,9 +1,34 @@
 from lib.common_model import BaseModel
+from lib.regex import *
+
+from typing import List
+
+from pydantic import Field, HttpUrl
+
+from beanie import Link
+
+
+class Address(BaseModel):
+    title: str = Field(min_length=1)
+    post_code: str | None = None
+    line_1: str = Field(min_length=1)
+    line_2: str | None = None
+    phone: str | None = None
+
+    class Settings:
+        name = "addresses"
 
 
 class User(BaseModel):
-    name: str
-    description: str | None = None
+    first_name: str | None = None
+    last_name: str | None = None
+    username: str = Field(regex=only_letters)
+    phone_number: str | None = Field(regex=iran_phone_number)
+    email: str = Field(regex=email)
+    password: str = Field(min_length=1)
+    avatar: HttpUrl | None = None
+    verified: bool = False
+    addresses: List[Link[Address]] | None = None
 
     class Settings:
         name = "users"
